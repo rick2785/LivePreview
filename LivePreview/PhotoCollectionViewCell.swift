748c844dc4c027d1f84e78eca32401973df6d7ad
Rickey Hrabowskie <rickeyhrabowskie@Rickeys-MacBook-Pro.local>
2016-10-29 07:10:47 -0500

    first commit

---
+++ LivePreview/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  LivePreview
+//
+//  Created by Rickey Hrabowskie on 10/23/16.
+//  Copyright © 2016 Rickey Hrabowskie. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+}
